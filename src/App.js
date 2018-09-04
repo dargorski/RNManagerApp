6b57b0596a3a465938be2c83a7cddeca1f157941
Dariusz Górski <dariusz.gorski@makingwaves.com>
2018-09-04 14:38:02 +0200

    login form with redux

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyCE_FdBLR7sMH7Q7Tcw4YnxlBuh3DqEeRw',
+    authDomain: 'manager-15ee0.firebaseapp.com',
+    databaseURL: 'https://manager-15ee0.firebaseio.com',
+    projectId: 'manager-15ee0',
+    storageBucket: 'manager-15ee0.appspot.com',
+    messagingSenderId: '185921935406'
+  };
+
+  firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+          <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
